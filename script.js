33b7fc6f6b6ddcd8b151005884897906ff3e10c0
Sojib1217 <sojibhasan1888@gmail.com>
2025-08-28 03:12:42 +0600

    heart counter functionality setup complete

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 
+// heart counter button
+
+function heartcount(){
+     const heartCounter=parseInt(document.getElementById('heart-count').innerText)
+     const clicker=1;    
+     const finalCount=heartCounter+clicker;
+
+   document.getElementById('heart-count').innerText=finalCount;
+}
+
+
