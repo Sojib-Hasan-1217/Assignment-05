ed98002d647abdd55d2006f15804e35be9b2ab6b
Sojib1217 <sojibhasan1888@gmail.com>
2025-08-28 20:22:38 +0600

    call history functionality complete

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-
-// heart counter button
+ // // heart counter button
 
 function heartcount(){
      const heartCounter=parseInt(document.getElementById('heart-count').innerText)
@@ -8,5 +7,449 @@ function heartcount(){
 
    document.getElementById('heart-count').innerText=finalCount;
 }
+ 
+ const transactionData=[];
 
 
+  //  reuseible function
+  function getInnerText(name){
+    const innterText=document.getElementById(name).innerText;
+    return innterText;
+  }
+
+  // parseInt function
+  function convertToNum(value){
+    const converter=parseInt(value);
+    return converter;
+   
+  }
+  //  click functionality for card btn -1
+document.getElementById('call-btn-1').addEventListener('click',function(event){
+  event.preventDefault();
+   
+
+  const coinField=getInnerText('coin-field');
+  const totalCoin=convertToNum(coinField);
+  const callPrice=20;
+  const serviceName=getInnerText("service-name-1");
+  const serviceNumber=getInnerText("service-number-1")
+  const remainingTotalCoin=totalCoin - callPrice;
+  
+  if(remainingTotalCoin < 0){
+    alert('you do not enough coin to make a call')
+     return
+  }
+  document.getElementById('coin-field').innerText=remainingTotalCoin;
+
+  alert(`calling ${serviceName} 
+          ${serviceNumber}`)
+
+          // for transaction container
+          const data={
+            name:serviceName,
+            number:serviceNumber
+          }
+    transactionData.push(data);
+
+    const time=new Date().toLocaleTimeString();
+          const historyContainer=document.getElementById('history-container');
+          const divElement=document.createElement('div')
+          for(const element of transactionData){
+            divElement.innerHTML=`<div class="flex justify-between items-center p-2 mt-4 rounded-lg bg-gray-100">
+                          <div class='text-sm '>
+                              <h1 class='font-bold'>${data.name}</h1>
+                              <p>${data.number}</p>
+                          </div>
+                          <p>${time}</p>
+                      </div>`
+                    }
+                    historyContainer.appendChild(divElement)
+
+          
+
+   
+
+
+   
+})
+  //  click functionality for card btn -2
+
+document.getElementById('call-btn-2').addEventListener('click',function(event){
+  event.preventDefault();
+   
+
+  const coinField=getInnerText('coin-field');
+  const totalCoin=convertToNum(coinField);
+  const callPrice=20;
+  const serviceName=getInnerText("service-name-2");
+  const serviceNumber=getInnerText("service-number-2")
+  const remainingTotalCoin=totalCoin - callPrice;
+  
+  if(remainingTotalCoin < 0){
+    alert('you do not enough coin to make a call')
+     return
+  }
+  document.getElementById('coin-field').innerText=remainingTotalCoin;
+
+  alert(`calling ${serviceName} 
+          ${serviceNumber}`)
+
+
+
+
+           // for transaction container
+          const data={
+            name:serviceName,
+            number:serviceNumber
+          }
+    transactionData.push(data);
+    
+    const time=new Date().toLocaleTimeString();
+          const historyContainer=document.getElementById('history-container');
+          const divElement=document.createElement('div')
+          for(const element of transactionData){
+            divElement.innerHTML=`<div class="flex justify-between items-center p-2 mt-4 rounded-lg bg-gray-100">
+                          <div class='text-sm '>
+                              <h1 class='font-bold'>${data.name}</h1>
+                              <p>${data.number}</p>
+                          </div>
+                          <p>${time}</p>
+                      </div>`
+                    }
+                    historyContainer.appendChild(divElement)
+})
+  //  click functionality for card btn -3
+
+document.getElementById('call-btn-3').addEventListener('click',function(event){
+  event.preventDefault();
+   
+
+  const coinField=getInnerText('coin-field');
+  const totalCoin=convertToNum(coinField);
+  const callPrice=20;
+  const serviceName=getInnerText("service-name-3");
+  const serviceNumber=getInnerText("service-number-3")
+  const remainingTotalCoin=totalCoin - callPrice;
+  
+  if(remainingTotalCoin < 0){
+    alert('you do not enough coin to make a call')
+     return
+  }
+  document.getElementById('coin-field').innerText=remainingTotalCoin;
+
+  alert(`calling ${serviceName} 
+          ${serviceNumber}`)
+
+           // for transaction container
+          const data={
+            name:serviceName,
+            number:serviceNumber
+          }
+    transactionData.push(data);
+    
+    const time=new Date().toLocaleTimeString();
+          const historyContainer=document.getElementById('history-container');
+          const divElement=document.createElement('div')
+          for(const element of transactionData){
+            divElement.innerHTML=`<div class="flex justify-between items-center p-2 mt-4 rounded-lg bg-gray-100">
+                          <div class='text-sm '>
+                              <h1 class='font-bold'>${data.name}</h1>
+                              <p>${data.number}</p>
+                          </div>
+                          <p>${time}</p>
+                      </div>`
+                    }
+                    historyContainer.appendChild(divElement)
+})
+
+
+
+
+  //  click functionality for card btn -4
+
+document.getElementById('call-btn-4').addEventListener('click',function(event){
+  event.preventDefault();
+   
+
+  const coinField=getInnerText('coin-field');
+  const totalCoin=convertToNum(coinField);
+  const callPrice=20;
+  const serviceName=getInnerText("service-name-4");
+  const serviceNumber=getInnerText("service-number-4")
+  const remainingTotalCoin=totalCoin - callPrice;
+  
+  if(remainingTotalCoin < 0){
+    alert('you do not enough coin to make a call')
+     return
+  }
+  document.getElementById('coin-field').innerText=remainingTotalCoin;
+
+  alert(`calling ${serviceName} 
+          ${serviceNumber}`)
+           // for transaction container
+          const data={
+            name:serviceName,
+            number:serviceNumber
+          }
+    transactionData.push(data);
+    
+    const time=new Date().toLocaleTimeString();
+          const historyContainer=document.getElementById('history-container');
+          const divElement=document.createElement('div')
+          for(const element of transactionData){
+            divElement.innerHTML=`<div class="flex justify-between items-center p-2 mt-4 rounded-lg bg-gray-100">
+                          <div class='text-sm '>
+                              <h1 class='font-bold'>${data.name}</h1>
+                              <p>${data.number}</p>
+                          </div>
+                          <p>${time}</p>
+                      </div>`
+                    }
+                    historyContainer.appendChild(divElement)
+})
+
+
+
+
+  //  click functionality for card btn -5
+
+document.getElementById('call-btn-5').addEventListener('click',function(event){
+  event.preventDefault();
+   
+
+  const coinField=getInnerText('coin-field');
+  const totalCoin=convertToNum(coinField);
+  const callPrice=20;
+  const serviceName=getInnerText("service-name-5");
+  const serviceNumber=getInnerText("service-number-5")
+  const remainingTotalCoin=totalCoin - callPrice;
+  
+  if(remainingTotalCoin < 0){
+    alert('you do not enough coin to make a call')
+     return
+  }
+  document.getElementById('coin-field').innerText=remainingTotalCoin;
+
+  alert(`calling ${serviceName} 
+          ${serviceNumber}`)
+           // for transaction container
+          const data={
+            name:serviceName,
+            number:serviceNumber
+          }
+    transactionData.push(data);
+    
+    const time=new Date().toLocaleTimeString();
+          const historyContainer=document.getElementById('history-container');
+          const divElement=document.createElement('div')
+          for(const element of transactionData){
+            divElement.innerHTML=`<div class="flex justify-between items-center p-2 mt-4 rounded-lg bg-gray-100">
+                          <div class='text-sm '>
+                              <h1 class='font-bold'>${data.name}</h1>
+                              <p>${data.number}</p>
+                          </div>
+                          <p>${time}</p>
+                      </div>`
+                    }
+                    historyContainer.appendChild(divElement)
+})
+
+
+
+
+  //  click functionality for card btn -6
+
+document.getElementById('call-btn-6').addEventListener('click',function(event){
+  event.preventDefault();
+   
+
+  const coinField=getInnerText('coin-field');
+  const totalCoin=convertToNum(coinField);
+  const callPrice=20;
+  const serviceName=getInnerText("service-name-6");
+  const serviceNumber=getInnerText("service-number-6")
+  const remainingTotalCoin=totalCoin - callPrice;
+  
+  if(remainingTotalCoin < 0){
+    alert('you do not enough coin to make a call')
+     return
+  }
+  document.getElementById('coin-field').innerText=remainingTotalCoin;
+
+  alert(`calling ${serviceName} 
+          ${serviceNumber}`)
+
+           // for transaction container
+          const data={
+            name:serviceName,
+            number:serviceNumber
+          }
+    transactionData.push(data);
+    
+    const time=new Date().toLocaleTimeString();
+          const historyContainer=document.getElementById('history-container');
+          const divElement=document.createElement('div')
+          for(const element of transactionData){
+            divElement.innerHTML=`<div class="flex justify-between items-center p-2 mt-4 rounded-lg bg-gray-100">
+                          <div class='text-sm '>
+                              <h1 class='font-bold'>${data.name}</h1>
+                              <p>${data.number}</p>
+                          </div>
+                          <p>${time}</p>
+                      </div>`
+                    }
+                    historyContainer.appendChild(divElement)
+})
+
+
+  //  click functionality for card btn -7
+
+document.getElementById('call-btn-7').addEventListener('click',function(event){
+  event.preventDefault();
+   
+
+  const coinField=getInnerText('coin-field');
+  const totalCoin=convertToNum(coinField);
+  const callPrice=20;
+  const serviceName=getInnerText("service-name-7");
+  const serviceNumber=getInnerText("service-number-7")
+  const remainingTotalCoin=totalCoin - callPrice;
+  
+  if(remainingTotalCoin < 0){
+    alert('you do not enough coin to make a call')
+     return
+  }
+  document.getElementById('coin-field').innerText=remainingTotalCoin;
+
+  alert(`calling ${serviceName} 
+          ${serviceNumber}`)
+
+           // for transaction container
+          const data={
+            name:serviceName,
+            number:serviceNumber
+          }
+    transactionData.push(data);
+    
+    const time=new Date().toLocaleTimeString();
+          const historyContainer=document.getElementById('history-container');
+          const divElement=document.createElement('div')
+          for(const element of transactionData){
+            divElement.innerHTML=`<div class="flex justify-between items-center p-2 mt-4 rounded-lg bg-gray-100">
+                          <div class='text-sm '>
+                              <h1 class='font-bold'>${data.name}</h1>
+                              <p>${data.number}</p>
+                          </div>
+                          <p>${time}</p>
+                      </div>`
+                    }
+                    historyContainer.appendChild(divElement)
+})
+
+
+  //  click functionality for card btn -8
+
+document.getElementById('call-btn-8').addEventListener('click',function(event){
+  event.preventDefault();
+   
+
+  const coinField=getInnerText('coin-field');
+  const totalCoin=convertToNum(coinField);
+  const callPrice=20;
+  const serviceName=getInnerText("service-name-8");
+  const serviceNumber=getInnerText("service-number-8")
+  const remainingTotalCoin=totalCoin - callPrice;
+  
+  if(remainingTotalCoin < 0){
+    alert('you do not enough coin to make a call')
+     return
+  }
+  document.getElementById('coin-field').innerText=remainingTotalCoin;
+
+  alert(`calling ${serviceName} 
+          ${serviceNumber}`)
+
+           // for transaction container
+          const data={
+            name:serviceName,
+            number:serviceNumber
+          }
+    transactionData.push(data);
+    
+    const time=new Date().toLocaleTimeString();
+          const historyContainer=document.getElementById('history-container');
+          const divElement=document.createElement('div')
+          for(const element of transactionData){
+            divElement.innerHTML=`<div class="flex justify-between items-center p-2 mt-4 rounded-lg bg-gray-100">
+                          <div class='text-sm '>
+                              <h1 class='font-bold'>${data.name}</h1>
+                              <p>${data.number}</p>
+                          </div>
+                          <p>${time}</p>
+                      </div>`
+                    }
+                    historyContainer.appendChild(divElement)
+})
+
+
+
+  //  click functionality for card btn -9
+
+document.getElementById('call-btn-9').addEventListener('click',function(event){
+  event.preventDefault();
+   
+
+  const coinField=getInnerText('coin-field');
+  const totalCoin=convertToNum(coinField);
+  const callPrice=20;
+  const serviceName=getInnerText("service-name-9");
+  const serviceNumber=getInnerText("service-number-9")
+  const remainingTotalCoin=totalCoin - callPrice;
+  
+  if(remainingTotalCoin < 0){
+    alert('you do not enough coin to make a call')
+     return
+  }
+  document.getElementById('coin-field').innerText=remainingTotalCoin;
+
+  alert(`calling ${serviceName} 
+          ${serviceNumber}`)
+
+ // for transaction container
+          const data={
+            name:serviceName,
+            number:serviceNumber
+          }
+    transactionData.push(data);
+    
+    const time=new Date().toLocaleTimeString();
+          const historyContainer=document.getElementById('history-container');
+          const divElement=document.createElement('div')
+          for(const element of transactionData){
+            divElement.innerHTML=`<div class="flex justify-between items-center p-2 mt-4 rounded-lg bg-gray-100">
+                          <div class='text-sm '>
+                              <h1 class='font-bold'>${data.name}</h1>
+                              <p>${data.number}</p>
+                          </div>
+                          <p>${time}</p>
+                      </div>`
+                    }
+                    historyContainer.appendChild(divElement)
+
+
+
+})
+
+// history delete btn handler
+document.getElementById('history-delete-btn').addEventListener('click',function(event){{
+  event.preventDefault();
+
+  document.getElementById('history-container').innerHTML=''
+}})
+  
+
+
+
+
+
+  
